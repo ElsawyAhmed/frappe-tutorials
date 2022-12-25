3fd1c396f4779d83be5248263d0dcb975a650b87
ElsawyAhmed <sawyahmed951@gmail.com>
2022-12-25 22:06:24 +0200

    report exam students

--- school_management/school_management/report/enrolled_students_grades/enrolled_students_grades.py
+++ school_management/school_management/report/enrolled_students_grades/enrolled_students_grades.py
@@ -5,11 +5,11 @@ import frappe
 
 
 def execute(filters=None):
-	exams = frappe.get_all('Course', fields=['*'])
+	exams = frappe.get_all('Exam Students', fields=['*'])
 	print("Exams", exams)
-	values = [[exam.get('name'), exam.get('level')] for exam in exams]
+	values = [[exam.get('exam'), exam.get('student'), exam.get('course'), exam.get('grade')] for exam in exams]
 	print('==============\n', values)
 	# enrolled_students = frappe.get_all('Enrolled Students', fields=['student', 'student_level', 'exam', 'grade'])
 	# print("Enrolled_students", enrolled_students)
-	columns, data = ['Student', 'Level', 'Exam', 'Grade'], []
+	columns, data = ['Exam', 'Student', 'Course', 'Grade'], values
 	return columns, data
